ce25c85c82e17b3e3fd96b8035207e5ba94d6f91
vysakh96 <118036322+vysakh96@users.noreply.github.com>
2023-01-18 21:06:15 +0530

    commit

---
+++ Toss_Up_Game/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'design_and_function.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const TossUp(),
+    ),
+  );
+}
+
